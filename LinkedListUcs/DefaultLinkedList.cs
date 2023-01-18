4b02196a1ccce5beef99b6c0002bc98dc427de3c
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-18 10:25:55 +0530

    [Ritesh]add : Ability to Crete simple Linked List

---
+++ LinkedListUcs/DefaultLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUcs
+{
+    public class DefaultLinkedList
+    {
+        public static void AddingElement()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item+" ");
+            }
+        }
+    }
+}
---
+++ LinkedListUcs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUcs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList UCs");
+            Console.WriteLine("Please chooes as per given Option \n " +
+                "1: Adding Element by BuiltIn-LinkedList\n" +
+                "2:Adding element by using custom LinkedList\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    DefaultLinkedList.AddingElement();
+                    break;  
+                case 2:
+                    CustomLinkedList customLinkedList = new CustomLinkedList();
+                    customLinkedList.AddLast(56);
+                    customLinkedList.AddLast(30);
+                    customLinkedList.AddLast(70);
+                    customLinkedList.Display();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option Selected .Please try again");
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
